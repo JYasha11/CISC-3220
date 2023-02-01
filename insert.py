00206127fdb1f32676313ad69d4036971457ca96
Jon <jonathanyashayev11@gmail.com>
2023-02-01 13:48:56 -0500

    Descending Insertion Sort

---
+++ insert.py
@@ -0,0 +1,15 @@
+def insertionSort(array):
+    for j in range(1, len(array)):
+        key = array[j]
+        i = j - 1
+        
+        while i >= 0 and array[i] < key : 
+            array[i + 1] = array[i]
+            i = i - 1
+        
+            array[i + 1] = key
+
+
+data = [2,8,4,3,1,6,3]
+insertionSort(data)
+print('Sorted Array in Descending Order:' , data)
