68fd61bf8d8691be8a970d641cfb3613e4d0486c
Jon <jonathanyashayev11@gmail.com>
2023-02-01 13:51:29 -0500

    Ascending & Descending

--- insert.py
+++ insert.py
@@ -1,4 +1,4 @@
-def insertionSort(array):
+def reverseInsertionSort(array):
     for j in range(1, len(array)):
         key = array[j]
         i = j - 1
@@ -9,7 +9,23 @@ def insertionSort(array):
         
             array[i + 1] = key
 
+def insertionSort(array):
+    for j in range(1, len(array)):
+        key = array[j]
+        i = j - 1
+        
+        while i >= 0 and array[i] > key : 
+            array[i + 1] = array[i]
+            i = i - 1
+        
+            array[i + 1] = key
+
 
 data = [2,8,4,3,1,6,3]
-insertionSort(data)
-print('Sorted Array in Descending Order:' , data)
+
+#reverseInsertionSort(data)
+#print('Sorted Array in Descending Order:' , data)
+
+print('Sorted Array in Descending Order:' , reverseInsertionSort(data))
+print('Sorted Array in Ascending Order:' , insertionSort(data))
+
