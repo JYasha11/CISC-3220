a57f161a4656dc9678b9da423741504bc9fb52f2
brooky11 <jonathanyashayev11@gmail.com>
2023-02-12 18:40:07 -0500

    Update insertion sort
    Made code more legible.

--- Assignment_1/insert.py
+++ Assignment_1/insert.py
@@ -2,32 +2,32 @@ def reverseInsertionSort(A):
     for j in range(1, len(A)):
         key = A[j]
         i = j - 1
-        
-        while i >= 0 and A[i] < key : 
+
+        while i >= 0 and A[i] < key:
             A[i + 1] = A[i]
             i = i - 1
-        
+
             A[i + 1] = key
 
-def insertionSort(array):
-    for j in range(1, len(array)):
-        key = array[j]
+
+def insertionSort(A):
+    for j in range(1, len(A)):
+        key = A[j]
         i = j - 1
-        
-        while i >= 0 and array[i] > key : 
-            array[i + 1] = array[i]
+
+        while i >= 0 and A[i] > key:
+            A[i + 1] = A[i]
             i = i - 1
-        
-            array[i + 1] = key
+
+            A[i + 1] = key
 
 
-A = [2,8,4,3,1,6,3]
+A = [2, 8, 4, 3, 1, 6, 3]
 
 reverseInsertionSort(A)
 
-print('Sorted Array in Descending Order:' , A)
+print('Sorted Array in Descending Order:', A)
 
-insertionSort(data)
+insertionSort(A)
 
-print('Sorted Array in Ascending Order:' , data)
-
+print('Sorted Array in Ascending Order:', A)
